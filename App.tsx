9c0c4effd542d60eb0d7255de5b8bfd1bdba616f
Poonam Dubey <poonamdubey1294@gmail.com>
2022-08-02 09:58:43 +0530

    Addded customer app layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import PhoneNumberConfirmationView from "./src/screens/PhoneConfimationView";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+ type StackParamList = {
+   PhoneNumber: undefined;
+ };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ const Stack = createNativeStackNavigator<StackParamList>();
+
+ return (
+   <NavigationContainer>
+     <Stack.Navigator
+       initialRouteName="PhoneNumber"
+       screenOptions={{
+         headerShown: false,
+       }}
+     >
+       <Stack.Screen name="PhoneNumber" component={PhoneNumberConfirmationView} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
+};
+
+export default App;
---
+++ README.md
@@ -0,0 +1,14 @@
+# sosh-customer-app
+for customer to register and access the emergency services
+
+# How to run te app
+1. Clone the repo
+2. run npm install on root folder
+3. run npm start
+4. Server up at http://localhost:19002/
+
+# To see runnin app
+1. Download expoo app in your phone.
+2. Change the connection 'LAN' from the browser at http://localhost:19002/  to 'Tunnel'.
+3. Scan the QR code appear at te bottom from expo app.
+4. Your app is good to go.
